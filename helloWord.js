67e88a360e4dcb639efa0ab157318ac1f97421ef
Nikita Kakurin <neyasut87@rambler.ru>
2017-05-14 08:26:51 +0300

    first commit

--- helloWord.js
+++ helloWord.js
@@ -1,27 +1,55 @@
 var express=require('express');
+var fortune = require('./lib/fortune.js');
 var app=express();
-app.set('port',process.env.PORT||3000);
-var handlebars=require('express-handlebars').create({defaultLayaut:'main'});
-var fortunes = ["Победи свои страхи, или они победят тебя.","Рекам нужны истоки.",
-    "Не бойся неведомого.","Тебя ждет приятный сюрприз.","Будь проще везде, где только можно." ]
+
+var handlebars=require('express-handlebars').create({defaultLayout:'main',
+	helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+app.set('port',process.env.PORT||3000);
+
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next(); 
+}); 
+
 app.use(express.static(__dirname + '/public'));
-	app.get('/',function(req, res){
-		res.render('home');
-	});
-	app.get('/about', function(req, res){  
-	  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]; 
-	     res.render('about', { fortune: randomFortune }); });
-	app.use('404',function(req, res,next){
-		res.status(404)
-		res.render('404');
-	});
-	app.get('500',function(err,req, res,next){
-		console.log(err.stack);
-		res.status(500);
-		res.render('500');
-	});
+
+app.get('/',function(req, res){
+	res.render('home');
+});
+
+app.get('/about',function(req, res){
+	res.render('about', { 
+		fortune: fortune.getFortune(), 
+ 		pageTestScript: '/qa/tests-about.js'
+	}); 
+});
+
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river'); 
+}); 
+
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate'); 
+});
+
+app.use('404',function(req, res,next){
+	res.status(404)
+	res.render('404');
+});
+app.get('500',function(err,req, res,next){
+	console.log(err.stack);
+	res.status(500);
+	res.render('500');
+});
 
 app.listen(app.get('port'),function(){
 	console.log('express was run on http://localhost:'+app.get('port'));
